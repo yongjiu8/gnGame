4e0f0f1b6c7d0fb31517146fb1ad66492577418c
永久 <[2584059921@qq.com]>
2023-12-11 23:16:10 +0800

    优化错误判断输出显示 添加自动完成任务 领取任务

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import base64
+import datetime
 import json
 import os
 import random
+import sys
 import time
 
 import requests
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+from urllib.parse import quote, unquote
 
-
+def printf(text, userId=''):
+    ti = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    print(f'[{ti}][{userId}]: {text}')
+    sys.stdout.flush()
 def aesDeToken(content, key='Hero590674\0\0\0\0\0\0'):
     key = key[:16].encode('utf-8')
     content = base64.b64decode(content)
@@ -51,7 +57,7 @@ def initGameConfig():
     }
     res = requests.get(url=url, headers=head).text
     js = json.loads(res)
-    print(res)
+    printf(res)
     flows = js['flows']
     for key in flows.keys():
         item = flows[key]
@@ -69,7 +75,59 @@ def initGameConfig():
             initConfig['iChartId'] = key
             initConfig['iSubChartId'] = key
             initConfig['sIdeToken'] = sIdeToken
+        elif sName == '[h5]分享':
+            shareConfig['iChartId'] = key
+            shareConfig['iSubChartId'] = key
+            shareConfig['sIdeToken'] = sIdeToken
+        elif sName == '[H5]任务领奖':
+            taskConfig['iChartId'] = key
+            taskConfig['iSubChartId'] = key
+            taskConfig['sIdeToken'] = sIdeToken
 
+def shareDay():
+    url = 'https://comm.ams.game.qq.com/ide/'
+    head = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': ck
+    }
+    data = {
+        'iChartId': shareConfig['iChartId'],
+        'iSubChartId': shareConfig['iSubChartId'],
+        'sIdeToken': shareConfig['sIdeToken'],
+        'sArea': sArea,
+        'sPlatId': sPlatId,
+        'acctype': acctype,
+        'e_code': '0',
+        'g_code': '0',
+        'eas_url': 'http%3A%2F%2Fgn.qq.com%2Fact%2Fa20231103through2%2F',
+        'eas_refer': 'http%3A%2F%2Fgn.qq.com%2Fact%2Fa20231103through2%2F%3Freqid%3Ded30a95a-0fe0-4ceb-8c7b-81980a724bc2%26version%3D27'
+    }
+    res = requests.post(url=url, headers=head, data=data).text
+    js = json.loads(res)
+    printf(res)
+
+def taskRecive(taskId):
+    url = 'https://comm.ams.game.qq.com/ide/'
+    head = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': ck
+    }
+    data = {
+        'iChartId': taskConfig['iChartId'],
+        'iSubChartId': taskConfig['iSubChartId'],
+        'sIdeToken': taskConfig['sIdeToken'],
+        'iTaskId': taskId,
+        'sArea': sArea,
+        'sPlatId': sPlatId,
+        'acctype': acctype,
+        'e_code': '0',
+        'g_code': '0',
+        'eas_url': 'http%3A%2F%2Fgn.qq.com%2Fact%2Fa20231103through2%2F',
+        'eas_refer': 'http%3A%2F%2Fgn.qq.com%2Fact%2Fa20231103through2%2F%3Freqid%3Ded30a95a-0fe0-4ceb-8c7b-81980a724bc2%26version%3D27'
+    }
+    res = requests.post(url=url, headers=head, data=data).text
+    js = json.loads(res)
+    printf(res)
 def init():
     url = 'https://comm.ams.game.qq.com/ide/'
     head = {
@@ -90,7 +148,13 @@ def init():
     }
     res = requests.post(url=url, headers=head, data=data).text
     js = json.loads(res)
-    print(res)
+    if js['ret'] == 101:
+        printf(js['sMsg'])
+        exit()
+    if js['ret'] == 1:
+        printf(js['sMsg'] + '查询游戏角色为空，未注册游戏或平台选错，请手动修改main.py最下面的代码选择安卓 ios端及wx或qq区')
+        exit()
+    return js
 
 def startGame():
     url = 'https://comm.ams.game.qq.com/ide/'
@@ -112,13 +176,16 @@ def startGame():
     }
     res = requests.post(url=url, headers=head, data=data).text
     js = json.loads(res)
-    print(res)
-    return js['jData']['nonce']
+    printf(res)
+    try:
+        return js['jData']['nonce']
+    except Exception:
+        printf('开始游戏失败，未注册游戏或平台选错，请手动修改main.py最下面的代码选择安卓 ios端及wx或qq区')
 
 def postScore(score=400):
     noce = aesEnToken(startGame())
-    print("玩游戏20秒，请耐心等待...")
-    time.sleep(20)
+    printf("玩游戏5秒，请耐心等待...")
+    time.sleep(5)
     url = 'https://comm.ams.game.qq.com/ide/'
     head = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -139,7 +206,10 @@ def postScore(score=400):
         'eas_refer': 'http%3A%2F%2Fgn.qq.com%2Fact%2Fa20231103through2%2F%3Freqid%3Da31baabc-8d98-48d0-8cad-070dd06fc4c1%26version%3D27'
     }
     res = requests.post(url=url, headers=head, data=data).text
-    print(res)
+    js = json.loads(res)
+    if js['ret'] == 0 and js['sMsg'] == 'succ':
+        printf(f'分数提交成功：{js["jData"]["score"]}分 可用总分：{js["jData"]["teamScore"]}分')
+    printf(res)
 
 if __name__ == '__main__':
     ck = getFileContent('./gnck.txt')
@@ -158,6 +228,17 @@ if __name__ == '__main__':
         'iSubChartId': '',
         'sIdeToken': '',
     }
+    shareConfig = {
+        'iChartId': '',
+        'iSubChartId': '',
+        'sIdeToken': '',
+    }
+    taskConfig = {
+        'iChartId': '',
+        'iSubChartId': '',
+        'sIdeToken': '',
+    }
+
     '''
     平台 0=ios 1=安卓
     '''
@@ -175,5 +256,25 @@ if __name__ == '__main__':
     '''
     sumbitScore = 400
     initGameConfig()
-    init()
-    postScore(sumbitScore)
+    res = init()
+    if res['ret'] == 0 and res['sMsg'] == 'succ':
+        jData = res['jData']
+        arrTask = jData['arrTask']
+        nickName = jData['nickName']
+        printf(f'欢迎英雄使用[{unquote(nickName)}]')
+        printf(f'开始做每天分享任务')
+        shareDay()
+        for it in arrTask:
+            name = it['name']
+            reddotTaskId = it['reddotTaskId']
+            printf(f'开始领取任务奖励 [{name}]')
+            taskRecive(reddotTaskId)
+        res = init()
+        ticket = res['jData']['ticket']
+        printf(f'任务完成 可突围次数：{ticket} 开始玩游戏提交成绩')
+        for i in range(1, int(ticket)+1):
+            postScore(sumbitScore)
+            printf(f'第{i}次提交成绩完成')
+        res = init()
+        teamScore = res['jData']['teamScore']
+        printf(f'执行完毕 可用总积分：{teamScore}')
